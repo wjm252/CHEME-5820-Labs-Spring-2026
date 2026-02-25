f9321454c8c210eb8dc331a24123a593d4d0138b
Jeffrey D Varner <jdv27@cornell.edu>
2026-02-25 16:18:15 -0500

    update
    update

---
+++ labs/week-6/L6d/Include.jl
@@ -0,0 +1,37 @@
+# setup paths -
+const _ROOT = @__DIR__
+
+# load external packages -
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# using statements -
+using VLDataScienceMachineLearningPackage
+using Images
+using ImageInTerminal
+using FileIO
+using ImageIO
+using OneHotArrays
+using Statistics
+using JLD2
+using LinearAlgebra
+using Plots
+using Colors
+using Distances
+using DataStructures
+using Test
+using Random
+using LinearAlgebra
+using Printf
+using NNlib
+using DataFrames
+using StatsPlots
+using StatsBase
+using Distributions
+
+
+# set the random seed for reproducibility
+Random.seed!(1234);
